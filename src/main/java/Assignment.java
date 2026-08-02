814c10f631862f6064e5f838005abb702b49c714
Estefan Vicencio <esvicencio@csumb.edu>
2026-08-02 10:19:23 -0700

    Added assignment constructors

--- src/main/java/Assignment.java
+++ src/main/java/Assignment.java
@@ -16,4 +16,44 @@ public class Assignment {
   private LocalDate dueDate;
   private double pointsPossible;
   private String status;
+
+  /**
+   * Makes an assignment that is already in the database.
+   */
+  public Assignment(
+      int assignmentId,
+      int classId,
+      String title,
+      String description,
+      LocalDate dueDate,
+      double pointsPossible,
+      String status) {
+
+    this.assignmentId = assignmentId;
+    this.classId = classId;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.pointsPossible = pointsPossible;
+    this.status = status;
+  }
+
+  /**
+   * Makes a new assignment before it is saved.
+   */
+  public Assignment(
+      int classId,
+      String title,
+      String description,
+      LocalDate dueDate,
+      double pointsPossible,
+      String status) {
+
+    this.classId = classId;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.pointsPossible = pointsPossible;
+    this.status = status;
+  }
 }
